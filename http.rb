17a8bf5ba0b0bf4abc0a4c55f85cd27427ba433a
siwakotisurya <siwakotisurya@outlook.com>
2015-10-02 16:38:34 +0545

    format code with tab index 2 and new function error_handler added to handle response error

--- http.rb
+++ http.rb
@@ -8,39 +8,48 @@ class GoogleSearch
 
 	def json_parser
 		puts "just a moment please system is handling your request"
-		puts "."*10
-		find_keyword = self.class.get("/weather?q=#{@location}.json")
-		@json = JSON.parse(find_keyword.body)
+		puts "."*20
+		@json = self.class.get("/weather?q=#{@location}.json")
+	end
+
+	def error_handler
+		user_ask
+		json_parser
+		case @json.code
+			when 200
+				puts @json.code
+				find
+			when 404
+				puts "sorry nothing match"
+		end
 	end
 
 	def find
 		puts "Todays Weather for #{@location} is Below"
-		puts "Longitude :  #{@json["coord"]["lon"]}"
-		puts "Latitude: #{@json["coord"]["lat"]}"
+		puts "Longitude:#{@json["coord"]["lon"]}"
+		puts "Latitude:#{@json["coord"]["lat"]}"
 		puts "Current Temperature: #{@json["main"]["temp"]-272.15} degree celcius"
-		puts "Counrty: #{@json["sys"]["country"]}"
-		puts "Humidity: #{@json["main"]["humidity"]}%"
+		puts "country: #{@json["sys"]["country"]}"
+		puts "humidity: #{@json["main"]["humidity"]}%"
 		puts "Wind Spped: #{@json["wind"]["speed"]}m/s"
 		@json["weather"].each do |s|
-			puts "Cloud:  #{s["description"]}"
+			puts "Cloud:#{s["description"]}"
 		end
-		puts "HTTP Resonse Status Code #{@json["cod"]}"
+		puts "HTTP Resonse Status Code: #{@json["cod"]} #{@json.message}"
 	end
 
 	def user_ask
 		puts "Enter the city name for example kathmandu, london, delhi"
 		input = gets.chomp
-		if input == Fixnum
-			puts "Only string is allowed"
-		else
+		if input.is_a?(String)
 			@location = input
+		else
+			puts "Only string is allowed"
 		end
  	end
 end
 
 search = GoogleSearch.new
 
-search.user_ask
-search.json_parser
-search.find
+search.error_handler
 
