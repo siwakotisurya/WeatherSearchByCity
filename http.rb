3f7820eba6fd6bd878a0302d9fc49a86c0008fd7
siwakotisurya <siwakotisurya@outlook.com>
2015-10-02 15:09:00 +0545

    a new function added json_parser in http.rb file this function separates a parsing function and only perform json response parsing task

--- http.rb
+++ http.rb
@@ -6,18 +6,25 @@ class GoogleSearch
 
 	base_uri "http://api.openweathermap.org/data/2.5"
 
-	def find
+	def json_parser
 		puts "just a moment please system is handling your request"
 		puts "."*10
 		find_keyword = self.class.get("/weather?q=#{@location}.json")
-		json = JSON.parse(find_keyword.body)
-		puts json
-		puts "Longitude :  #{json["coord"]["lon"]}"
-		puts "Latitude: #{json["coord"]["lat"]}"
-		puts "Current Temperature: #{json["main"]["temp"]-272.15} degree celcius"
-		puts "Counrty: #{json["sys"]["country"]}"
-		puts "Humidity: #{json["main"]["humidity"]}%"
-		puts "Wind Spped: #{json["wind"]["speed"]}m/s"
+		@json = JSON.parse(find_keyword.body)
+	end
+
+	def find
+		puts "Todays Weather for #{@location} is Below"
+		puts "Longitude :  #{@json["coord"]["lon"]}"
+		puts "Latitude: #{@json["coord"]["lat"]}"
+		puts "Current Temperature: #{@json["main"]["temp"]-272.15} degree celcius"
+		puts "Counrty: #{@json["sys"]["country"]}"
+		puts "Humidity: #{@json["main"]["humidity"]}%"
+		puts "Wind Spped: #{@json["wind"]["speed"]}m/s"
+		@json["weather"].each do |s|
+			puts "Cloud:  #{s["description"]}"
+		end
+		puts "HTTP Resonse Status Code #{@json["cod"]}"
 	end
 
 	def user_ask
@@ -34,6 +41,6 @@ end
 search = GoogleSearch.new
 
 search.user_ask
-
+search.json_parser
 search.find
 
